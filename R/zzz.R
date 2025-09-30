0e8b698da01a9ee95881f27427cfd43e7bc0d2bc
Alex Bates <alexander.shakeel.bates@gmail.com>
2025-09-29 20:20:41 -0400

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 # Ensure g.anat is the canonical object when the namespace loads (CI-safe)
 .onLoad <- function(libname, pkgname) {
-  assign("g.anat", .build_g_anat(), envir = asNamespace(pkgname))
+  assign("gganat", .build_g_anat(), envir = asNamespace(pkgname))
 }
 
