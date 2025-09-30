472c6c68c97a1d8e53b5ef49bb50cdc4bfa296d6
Alex Bates <alexander.shakeel.bates@gmail.com>
2025-09-29 21:13:57 -0400

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -107,6 +107,34 @@ is_named_all <- function(x, require_unique = FALSE) {
   ok
 }
 
+
+#' @noRd
+.build_gganat <- function() {
+  ggplot2::ggplot() +
+    ggplot2::coord_fixed() +                 # required by tests
+    ggplot2::theme_void() +                  # blank canvas
+    ggplot2::guides(fill = "none", colour = "none") +
+    ggplot2::theme(
+      legend.position   = "none",
+      plot.title        = ggplot2::element_text(hjust = 0, size = 8, face = "bold", colour = "black"),
+      axis.title.x      = ggplot2::element_blank(),
+      axis.text.x       = ggplot2::element_blank(),
+      axis.ticks.x      = ggplot2::element_blank(),
+      axis.title.y      = ggplot2::element_blank(),
+      axis.text.y       = ggplot2::element_blank(),
+      axis.ticks.y      = ggplot2::element_blank(),
+      axis.line         = ggplot2::element_blank(),
+      panel.grid.major  = ggplot2::element_blank(),
+      panel.grid.minor  = ggplot2::element_blank(),
+      plot.margin       = ggplot2::margin(0, 0, 0, 0),
+      panel.spacing     = ggplot2::unit(0, "cm"),
+      panel.border      = ggplot2::element_blank(),
+      panel.background  = ggplot2::element_blank(),
+      plot.background   = ggplot2::element_blank()
+    ) +
+    ggplot2::labs(title = "")
+}
+
 #' Base ggplot2 Template for Neuroanatomy Plots
 #'
 #' @description
@@ -149,32 +177,3 @@ is_named_all <- function(x, require_unique = FALSE) {
 #' @seealso \code{\link{geom_neuron}}, \code{\link{ggneuron}}
 #' @export
 gganat <- .build_gganat()
-
-#' @noRd
-.build_gganat <- function() {
-  ggplot2::ggplot() +
-    ggplot2::coord_fixed() +                 # required by tests
-    ggplot2::theme_void() +                  # blank canvas
-    ggplot2::guides(fill = "none", colour = "none") +
-    ggplot2::theme(
-      legend.position   = "none",
-      plot.title        = ggplot2::element_text(hjust = 0, size = 8, face = "bold", colour = "black"),
-      axis.title.x      = ggplot2::element_blank(),
-      axis.text.x       = ggplot2::element_blank(),
-      axis.ticks.x      = ggplot2::element_blank(),
-      axis.title.y      = ggplot2::element_blank(),
-      axis.text.y       = ggplot2::element_blank(),
-      axis.ticks.y      = ggplot2::element_blank(),
-      axis.line         = ggplot2::element_blank(),
-      panel.grid.major  = ggplot2::element_blank(),
-      panel.grid.minor  = ggplot2::element_blank(),
-      plot.margin       = ggplot2::margin(0, 0, 0, 0),
-      panel.spacing     = ggplot2::unit(0, "cm"),
-      panel.border      = ggplot2::element_blank(),
-      panel.background  = ggplot2::element_blank(),
-      plot.background   = ggplot2::element_blank()
-    ) +
-    ggplot2::labs(title = "")
-}
-
-
