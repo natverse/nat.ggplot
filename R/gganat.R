5ee49adbfd9b4c9a32267eab344e9bea471336b2
Alex Bates <alexander.shakeel.bates@gmail.com>
2025-09-29 23:40:27 -0400

    Update gganat.R

--- R/gganat.R
+++ R/gganat.R
@@ -1,3 +1,30 @@
+# hidden
+.build_gganat <- function() {
+  ggplot2::ggplot() +
+    ggplot2::coord_fixed() +                 # tests expect CoordFixed present
+    ggplot2::theme_void() +                  # ensures a theme is present
+    ggplot2::guides(fill = "none", colour = "none") +
+    ggplot2::theme(
+      legend.position   = "none",
+      plot.title        = ggplot2::element_text(hjust = 0, size = 8, face = "bold", colour = "black"),
+      axis.title.x      = ggplot2::element_blank(),
+      axis.text.x       = ggplot2::element_blank(),
+      axis.ticks.x      = ggplot2::element_blank(),
+      axis.title.y      = ggplot2::element_blank(),
+      axis.text.y       = ggplot2::element_blank(),
+      axis.ticks.y      = ggplot2::element_blank(),
+      axis.line         = ggplot2::element_blank(),
+      panel.grid.major  = ggplot2::element_blank(),
+      panel.grid.minor  = ggplot2::element_blank(),
+      plot.margin       = ggplot2::margin(0, 0, 0, 0),
+      panel.spacing     = ggplot2::unit(0, "cm"),
+      panel.border      = ggplot2::element_blank(),
+      panel.background  = ggplot2::element_blank(),
+      plot.background   = ggplot2::element_blank()
+    ) +
+    ggplot2::labs(title = "")
+}
+
 #' Base ggplot2 Template for Neuroanatomy Plots
 #'
 #' @description
@@ -23,54 +50,10 @@
 #'
 #' @examples
 #' \dontrun{
-#' gganat +
-#'   nat.ggplot::geom_neuron(banc.skels, rotation_matrix = banc_view)
-#'
-#' gganat +
-#'   nat.ggplot::geom_neuron(
-#'     banc.brain_neuropil, rotation_matrix = banc_view,
-#'     cols = c("grey95","grey85"), alpha = 0.3
-#'   ) +
-#'   nat.ggplot::geom_neuron(
-#'     banc.skels, rotation_matrix = banc_view,
-#'     cols = c("purple","magenta")
-#'   )
+#' gganat + nat.ggplot::geom_neuron(banc.skels, rotation_matrix = banc_view)
 #' }
 #'
 #' @seealso \code{\link{geom_neuron}}, \code{\link{ggneuron}}
-NULL
-
-# ---- Internal builder ---------------------------------------------------------
-
-#' @noRd
-.build_gganat <- function() {
-  ggplot2::ggplot() +
-    ggplot2::coord_fixed() +                 # tests expect CoordFixed
-    ggplot2::theme_void() +                  # ensures a theme is present
-    ggplot2::guides(fill = "none", colour = "none") +
-    ggplot2::theme(
-      legend.position   = "none",
-      plot.title        = ggplot2::element_text(hjust = 0, size = 8, face = "bold", colour = "black"),
-      axis.title.x      = ggplot2::element_blank(),
-      axis.text.x       = ggplot2::element_blank(),
-      axis.ticks.x      = ggplot2::element_blank(),
-      axis.title.y      = ggplot2::element_blank(),
-      axis.text.y       = ggplot2::element_blank(),
-      axis.ticks.y      = ggplot2::element_blank(),
-      axis.line         = ggplot2::element_blank(),
-      panel.grid.major  = ggplot2::element_blank(),
-      panel.grid.minor  = ggplot2::element_blank(),
-      plot.margin       = ggplot2::margin(0, 0, 0, 0),
-      panel.spacing     = ggplot2::unit(0, "cm"),
-      panel.border      = ggplot2::element_blank(),
-      panel.background  = ggplot2::element_blank(),
-      plot.background   = ggplot2::element_blank()
-    ) +
-    ggplot2::labs(title = "")
-}
-
-# ---- Public object ------------------------------------------------------------
-
 #' @export
 gganat <- .build_gganat()
 
